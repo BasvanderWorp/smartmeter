579c18ff66cca5eef50dd0dd4ebe9b471b423a08
Bas van der Worp <bas.vanderworp@gmail.com>
2021-07-11 15:21:20 +0200

    logging and parameters added, work in progress

--- smartmeter.py
+++ smartmeter.py
@@ -1,26 +1,92 @@
-# DSMR P1 uitlezen
-# (c) 10-2012 - GJ - gratis te kopieren en te plakken
+# Dutch Smart Meter Reader (P1)
 versie = "1.0"
 import sys
 import serial
 
 ##############################################################################
+# Part 0.1: Set global variables
+##############################################################################
+PROGRAM_NAME = 'dsmr'
+PROGRAM_VERSION = "0.01"
+PROGRAM_VERSION_DATE = "11-07-2021"
+PROGRAM_AUTHOR = "Bas van der Worp"
+CONFIG_STORE = '/dsmr/dsmr_config.json'
+CONFIG = read_config(CONFIG_STORE)
+LOG_PATH_BASE = CONFIG['LOG_PATH_BASE']
+OUTPUT_PATH_BASE = CONFIG['OUTPUT_PATH_BASE']
+DMSR_PORT = CONFIG['DMSR_PORT']
+DMSR_PORT = "/dev/ttyUSB0"
+DMSR_BAUDRATE = CONFIG['DMSR_BAUDRATE']
+DMSR_BAUDRATE = 115200
+DMSR_BYTESIZE = CONFIG['DMSR_BYTESIZE']
+DMSR_BYTESIZE =serial.SEVENBITS
+DMSR_PARITY = CONFIG['DMSR_PARITY']
+DMSR_PARITY = serial.PARITY_EVEN
+
+##############################################################################
 # Main program
 ##############################################################################
-print ("DSMR P1 uitlezen",  versie)
-print ("Control-C om te stoppen")
-print ("Pas eventueel de waarde ser.port aan in het python script")
+if __name__ == '__main__':
+    ##########################################################################
+    # === Part 0.2: Commandline parameter initialisation
+    ##########################################################################
+    parser = argparse.ArgumentParser()
+    requiredNamed = parser.add_argument_group('required named arguments')
+    requiredNamed.add_argument("-d", "--dummy", help="Dummy")
+    args = parser.parse_args()
+
+    if args.dummy:
+        dummy = args.dummy
+    else:
+        dummy = ""
+
+    ##########################################################################
+    # Part 0.3: Initialise logging
+    ##########################################################################
+    # Check whether logfolder exists. If not, write to 'log' folder
+    LOG_PATH = f'{LOG_PATH_BASE}{PROGRAM_NAME}/'
+    if not os.path.exists(LOG_PATH):
+        try:
+            os.makedirs(LOG_PATH)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                LOG_PATH = ""
+                raise
+
+    OS_USER = os.getlogin().lower()
+    LOGLEVEL_DEBUG = eval('logging.DEBUG')
+    LOGLEVEL_INFO = eval('logging.INFO')
+
+    LOGFILE = os.path.normpath(LOG_PATH + "log"
+                               "_" + "{:%Y%m%d}".format(datetime.now()) +
+                               ".log")
+
+    logging.basicConfig(
+        filename=LOGFILE,
+        level='INFO',
+        format='%(asctime)s %(levelname)s ' +
+               '%(name)s %(funcName)s: %(message)s',
+        datefmt='%d-%m-%Y %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    msg_1 = '='*80
+    logger.info(msg_1)
+    logger.info('Start program    : ' + PROGRAM_NAME)
+    logger.info('Version          : ' + PROGRAM_VERSION)
+    logger.info('Version date     : ' + PROGRAM_VERSION_DATE)
+    logger.info('Host             : ' + socket.gethostname())
+    logger.info('parameters       : ')
+    logger.info('parameters       : ')
+
+print ("Digital Smart Meter Reader (P1), version ",  PROGRAM_VERSION)
+print ("Press Control-C to stop")
 
 # Set COM port config
-ser = serial.Serial("/dev/ttyUSB0")
-ser.baudrate = 115200
-ser.bytesize=serial.SEVENBITS
-# ser.parity=serial.PARITY_EVEN
-# ser.stopbits=serial.STOPBITS_ONE
-# ser.xonxoff=0
-# ser.rtscts=0
-# ser.timeout=1
-ser.port="/dev/ttyUSB0"
+ser = serial.Serial()
+ser.port = DMSR_PORT
+ser.baudrate = DMSR_BAUDRATE
+ser.bytesize = DMSR_BYTESIZE
+ser.parity = DMSR_PARITY
 
 # Open COM port
 import ipdb; ipdb.set_trace()
@@ -29,10 +95,12 @@ try:
     print('Seriele poort geopend!')
 except serial.serialutil.SerialException as err1:
     if err1.args[0] == 'Port is already open.':
+        logger.warning(err1)
         pass
     else:
-        sys.exit ("Fout bij het openen van %s. Aaaaarch."  % ser.name)
-
+        msg = f'Error opening port {ser.name}, error: {err1}'
+        logger.warning(msg)
+        print (msg, ' program terminated')
 
 # Initialize
 # p1_teller is mijn tellertje voor van 0 tot 20 te tellen
